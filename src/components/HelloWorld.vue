45c21abac87bd9a6e61aaaa789585f8b5839ac5e
Simon <simon.tessard@gmail.com>
2024-01-26 19:09:31 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="font-semibold text-2xl text-blue-500 px-4">Apple Project : {{ name }} </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'World'
+    };
+  }
+};
+</script>
